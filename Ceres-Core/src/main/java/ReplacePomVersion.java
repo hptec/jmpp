e4d42783e83c6e22b6f6376ebfc011af4aca1988
heyi <hptec@users.noreply.github.com>
2016-11-27 08:58:15 +0000

    : ID-HCJKQ
    Task-Url:

--- Ceres-Core/src/main/java/ReplacePomVersion.java
+++ Ceres-Core/src/main/java/ReplacePomVersion.java
@@ -16,7 +16,7 @@ import com.google.common.collect.Lists;
 
 public class ReplacePomVersion {
 
-	public static final String VERSION = "3.0.28";
+	public static final String VERSION = "3.0.30";
 
 	public static final String DPLOY_CMD = "mvn clean deploy -Dmaven.test.skip=true";
 
